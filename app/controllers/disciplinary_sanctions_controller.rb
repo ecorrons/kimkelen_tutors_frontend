f7b5d0defc55f11f4272cb8a7b3eb78d9d732cd7
María Emilia Corrons <ecorrons@cespi.unlp.edu.ar>
2015-10-08 00:25:33 -0300

    Nuevas vistas

--- app/controllers/disciplinary_sanctions_controller.rb
+++ app/controllers/disciplinary_sanctions_controller.rb
@@ -4,6 +4,20 @@ class DisciplinarySanctionsController < ApplicationController
   before_action :set_person
 
   def index
+    res = Api::call "/students/#{@student.id}/school_years"
+
+    if params[:school_year].nil?
+
+      @school_year = res['school_years'].last
+      @school_year = @school_year['year'].to_s
+    else
+      @school_year = params[:school_year]
+    end
+
+    res_disciplinary_sanctions = Api::call "/students/#{@student.id}/disciplinary_sanctions/#{@school_year}"
+
+    @school_years = res['school_years']
+    @disciplinary_sanctions = res_disciplinary_sanctions['disciplinary_sanctions_for_year']
     render 'students/disciplinary_sanctions'
   end
 
